9dbc195ac33805d6c1cec5dd6e116164fe07f308
Pavel Dzhagriev <pdzhagriev@dev.vtb>
2021-09-08 18:36:30 +0400

    red line test

--- MedianFilter.cpp
+++ MedianFilter.cpp
@@ -12,11 +12,22 @@ int main() {
 extern "C" {
 #endif
 
-EMSCRIPTEN_KEEPALIVE void myFunction(uint8_t *arr, int size) {
+EMSCRIPTEN_KEEPALIVE uint8_t* myFunction(uint8_t *arr, int size) {
     cout<<"My function called"<<endl;
     cout<<"Size: "<<size<<endl;
-    for (int cnt = 0; cnt < 36; cnt++) 
-       cout<<"arr["<<cnt<<"] = "<<(int) arr[cnt]<<endl;
+    for (int cnt = 0; cnt < 3200; cnt++) {
+        if (cnt%4 == 0) {
+            cout<<"arr["<<cnt<<"] = "<<(int) arr[cnt]<<endl;
+            arr[cnt] = 255;
+            if (cnt > 0) {
+                arr[cnt - 1] = 255;
+                arr[cnt - 2] = 0;
+                arr[cnt - 3] = 0;
+            }
+        }
+    }
+
+    return arr;
 }
 
 #ifdef __cplusplus
